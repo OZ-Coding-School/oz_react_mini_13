cb91c021949c37249d7405aa9ecfa4305121f9ef
이원주 <hgf9500@gmail.com>
2025-09-08 10:45:00 +0900

    feat: 미션 2 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import MovieCard from './components/MovieCard';
-import movieListData from './data/movieListData.json';
+
+// .env 파일에 저장된 API 토큰 값
+const TMDB_API_ACCESS_TOKEN = import.meta.env.VITE_TMDB_API_ACCESS_TOKEN;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    setMovies(movieListData.results);
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          'https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1',
+          {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: `Bearer ${TMDB_API_ACCESS_TOKEN}`, // API 토큰 사용
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error('API 호출에 실패했습니다.');
+        }
+
+        const data = await response.json();
+        
+        // adult 값이 false인 영화만 필터링
+        const filteredMovies = data.results.filter(movie => movie.adult === false);
+        setMovies(filteredMovies);
+        
+      } catch (error) {
+        console.error('영화 데이터를 가져오는 중 오류 발생:', error);
+      }
+    };
+
+    fetchMovies();
   }, []);
 
   return (
-    // 'bg-black' 클래스를 제거하여 부모 컴포넌트의 배경색을 따르게 합니다.
-    <div className="text-white p-4 md:p-8">
+    <div className="bg-slate-900 text-white p-4 md:p-8">
       <h2 className="text-xl md:text-2xl font-bold mb-6 mt-4">인기순</h2>
       <div className="grid grid-cols-4 gap-4 justify-items-center">
         {movies.length > 0 ? (
